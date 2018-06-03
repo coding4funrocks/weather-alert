4990cac6fa48d0ae967da23f4dc058dc88961f11
coding4funrocks <thefitgeekgirl@gmail.com>
2018-06-03 10:19:44 +0200

    Create Azure Functions with Visual Studio Code

---
+++ GetWeather/index.js
@@ -0,0 +1,13 @@
+const openWeatherMapApiUrl = process.env.OpenWeatherMapApiUrl;
+
+module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if(myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    context.log('JavaScript timer trigger function ran!:', timeStamp);   
+    
+    context.done();
+};
